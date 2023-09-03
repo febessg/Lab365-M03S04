ddda17951792324b0806347ea4ef80d432ba2f91
Fernanda Gonçalves <fernanda-bess@hotmail.com>
2023-09-03 20:32:10 -0300

    creating user CRUD

---
+++ src/controllers/user.controller.js
@@ -0,0 +1,97 @@
+const { sign } = require('jsonwebtoken');
+const {User} = require('../models/User');
+
+class UserController {
+    async create(req, res) {
+        try {
+            const {
+                name,
+                email,
+                password
+            } = req.body;
+
+            const userCreated = await User.create({
+                name,
+                email,
+                password
+            });
+
+            return res.status(201).send(userCreated)
+        } catch (error) {
+            return res.status(400).send({
+                message: 'Erro na criação do usuário',
+                cause: error.message
+            })            
+        }
+    }
+
+    async findAll(req, res) {
+        try {
+            const users = await User.findAll();
+
+            const usersToShow = users.map((user) => {
+                return {
+                    name: user.name,
+                    email: user.email
+                }
+            });
+
+            return res.status(200).send(usersToShow);
+        } catch (error) {
+            return res.status(400).send({
+                message: 'Erro ao listar todos usuários',
+                cause: error.message
+            }) 
+        }
+    }
+
+    async findOne(req, res) {
+        try {
+            const {userId} = req.params;
+            const user = await User.findByPk(userId);
+
+            return res.status(200).send(`Usuário: ${user.email} criado com sucesso`)
+        } catch (error) {
+            return res.status(400).send({
+                message: 'Erro ao listar o usuário',
+                cause: error.message
+            }) 
+        }
+    }
+
+    async login(req, res) {
+        try {
+            const { email, password } = req.body;
+
+            const user = await User.findOne({
+                where: {email}
+            });
+
+            if (!user) {
+                return res.status(400).send({
+                    message: 'Usuário não encontrado'
+                })
+            }
+
+            if (user.password === password) {
+                const payload = {
+                    name: user.name, 
+                    email: user.email
+                }
+                const token = sign(payload, process.env.JWT_SECRET)
+                return res.status(200).send({token})
+            } else {
+                return res.status(400).send({
+                    message: 'Senha inválida'
+                })
+            }
+        } catch (error) {
+            return res.status(400).send({
+                message: 'Erro ao realizar login do usuário',
+                cause: error.message
+            })            
+        }
+    }
+};
+
+module.exports = new UserController();
