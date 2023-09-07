1db1f063a0f21f1125d0c6e1f8ccff2901ff9582
Fernanda Gonçalves <fernanda-bess@hotmail.com>
2023-09-06 22:29:33 -0300

    creating migration + model productCart

---
+++ src/models/ProductCart.js
@@ -0,0 +1,23 @@
+const {INTEGER} = require('sequelize');
+const {connection} = require('../database/connection');
+
+const ProductCart = connection.define('productCart', {
+    productId: {
+        type: INTEGER,
+        references: {
+            model: {
+                tableName: 'product'
+            }
+        }
+    },
+    cartId: {
+        type: INTEGER,
+        references: {
+            model: {
+                tableName: 'cart'
+            }
+        }
+    }
+}, {underscored: true, paranoid: true});
+
+module.exports = { ProductCart }
